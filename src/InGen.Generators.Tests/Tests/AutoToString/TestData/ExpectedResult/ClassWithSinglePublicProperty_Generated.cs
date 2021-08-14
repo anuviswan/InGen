4c9f6b2d49d92135deab55c26d3678523fa328e8
Anu Viswan <anu.viswan@gmail.com>
2021-08-14 20:46:10 +0530

    added test cases

---
+++ src/InGen.Generators.Tests/Tests/AutoToString/TestData/ExpectedResult/ClassWithSinglePublicProperty_Generated.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InGen.Generators.Tests.Tests.AutoToString.TestCaseData.TestCase
+{
+    public partial class ClassWithSinglePublicProperty
+    {
+        public override string ToString()
+        {
+            return $"{nameof(UserName)}={UserName}";
+        }
+    }
+}
---
+++ src/InGen.Generators.Tests/Tests/AutoToString/TestData/TestCase/ClassWithSinglePublicProperty.cs
@@ -0,0 +1,10 @@
+﻿using InGen.Types.Attributes;
+
+namespace InGen.Generators.Tests.Tests.AutoToString.TestCaseData.TestCase
+{
+    [AutoToString]
+    public partial class ClassWithSinglePublicProperty
+    {
+        public string UserName { get; set; }
+    }
+}
